d8ffa2011145d065d5005a1fa5c9e90e142fa6a0
mr-brightsid3 <banot.jan@gmail.com>
2022-03-08 03:51:58 +0100

    add tests

---
+++ tests.py
@@ -0,0 +1,38 @@
+import unittest
+from interpreter import lisp_eval, parse
+
+
+class EvalTests(unittest.TestCase):
+
+    def test_addition(self):
+        test_procedure = "(+ 10 9)"
+        self.assertEqual(19, lisp_eval(parse(test_procedure)))
+
+    def test_subtraction(self):
+        test_procedure = "(- 10 9)"
+        self.assertEqual(1, lisp_eval(parse(test_procedure)))
+
+    def test_multiplication(self):
+        test_procedure = "(* 10 9)"
+        self.assertEqual(90, lisp_eval(parse(test_procedure)))
+
+    def test_division(self):
+        test_procedure = "(/ 90 9)"
+        self.assertEqual(10, lisp_eval(parse(test_procedure)))
+
+    def test_equality(self):
+        test_procedure = "(eq 90 9)"
+        self.assertFalse(lisp_eval(parse(test_procedure)))
+
+    def test_lists(self):
+        test_procedure = "(define box (cons 3 4))"
+        lisp_eval(parse(test_procedure))
+        test_procedure_cons = "box"
+        test_procedure_car = "(car box)"
+        test_procedure_cdr = "(cdr box)"
+        self.assertEqual([3, 4], lisp_eval(test_procedure_cons))
+        self.assertEqual(3, lisp_eval(parse(test_procedure_car)))
+        self.assertEqual(4, lisp_eval(parse(test_procedure_cdr)))
+
+if __name__ == '__main__':
+    unittest.main()
